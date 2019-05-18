3dfa8d2803a601cf0360566ec8d34cfc728ddd77
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2019-05-18 12:16:55 +0200

    feat: update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Eddybrando.com`,
+    description: ``,
+    author: `Eddybrando Vásquez`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `eddybrando`,
+        short_name: `eddybrando`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
