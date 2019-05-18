81781499f9bf7e7a8374225feae60707d1599e2f
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2019-05-18 17:53:36 +0200

    feat(home): display profile picture

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,35 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link, StaticQuery } from "gatsby"
+import Img from "gatsby-image"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <p>
-      I’m a Peruvian developer, designer and entrepreneur specialised in web
-      technologies.
-    </p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  <StaticQuery
+    query={graphql`
+      query {
+        image: file(relativePath: { eq: "eddybrando.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 200) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <Layout>
+        <SEO title="Home" />
+        <Img fluid={data.image.childImageSharp.fluid} />
+        <p>
+          I’m a Peruvian developer, designer and entrepreneur specialised in web
+          technologies.
+        </p>
+        <Link to="/page-2/">Go to page 2</Link>
+      </Layout>
+    )}
+  />
 )
 
 export default IndexPage
