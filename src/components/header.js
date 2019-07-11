db074616aec9c913f80e9741bb18802cdffecbe8
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2019-07-11 07:54:25 +0200

    fix(welcome): use "className"

--- src/components/header.js
+++ src/components/header.js
@@ -2,8 +2,8 @@ import React from "react"
 
 const Header = () => (
   <header className="header">
-    <nav class="nav" />
-    <section class="title">
+    <nav className="nav" />
+    <section className="title">
       <h1 className="headline">
         Hi, I'm <span className="highlight">Eddybrando</span>.
       </h1>
