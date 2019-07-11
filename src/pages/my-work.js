91042c6f334e3c30e82285172585911892494058
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2019-07-11 22:56:41 +0200

    feat(my-work): add copy

--- src/pages/my-work.js
+++ src/pages/my-work.js
@@ -7,11 +7,26 @@ import SEO from "../components/seo"
 const SecondPage = () => (
   <Layout
     headerTitle={{ prepend: "My ", highlight: "work" }}
+    mainContainerClass="main-container--full-height"
     nav={[{ label: "Home", link: "/" }]}
   >
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h2>How it began</h2>
+    <p>
+      I started coding around 2003 at age 13 and drafted my first web designs
+      when I was 14.
+    </p>
+
+    <p>
+      Back then, my tech stack was HTML 4, CSS 2 and Macromedia Flash 7, along
+      with the occasional JavaScript snippet.
+    </p>
+
+    <p>
+      My tools of choice were Macromedia Dreamweaver and Microsoft FrontPage for
+      code editing, and Yahoo! GeoCities or the free .tk domains host for
+      hosting.
+    </p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
