92d7cf31c6c498c149adec9016bf4ad9550cdb69
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2019-07-11 21:46:59 +0200

    feat(my-work): link page

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,28 @@
 import React from "react"
+import PropTypes from "prop-types"
 
-const Header = () => (
+const Header = ({ titleAppend, titleHighlight, titlePrepend }) => (
   <header className="header">
     <nav className="nav" />
     <section className="title">
       <h1 className="headline">
-        Hi, I'm <span className="highlight">Eddybrando</span>.
+        {titlePrepend}
+        <span className="highlight">{titleHighlight}</span>
+        {titleAppend}
       </h1>
     </section>
   </header>
 )
+
+Header.propTypes = {
+  titleAppend: PropTypes.string,
+  titleHighlight: PropTypes.string.isRequired,
+  titlePrepend: PropTypes.string,
+}
+
+Header.defaultProps = {
+  titleAppend: "",
+  titlePrepend: "",
+}
 
 export default Header
--- src/components/layout.js
+++ src/components/layout.js
@@ -3,15 +3,24 @@ import PropTypes from "prop-types"
 
 import Header from "./header"
 
-const Layout = ({ children, mainContainerClass }) => (
+const Layout = ({ children, headerTitle, mainContainerClass }) => (
   <div className="page-grid">
-    <Header />
+    <Header
+      titleAppend={headerTitle.append}
+      titleHighlight={headerTitle.highlight}
+      titlePrepend={headerTitle.prepend}
+    />
     <main className={`main-container ${mainContainerClass}`}>{children}</main>
   </div>
 )
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
+  headerTitle: PropTypes.shape({
+    append: PropTypes.string,
+    highlight: PropTypes.string.isRequired,
+    prepend: PropTypes.string,
+  }).isRequired,
   mainContainerClass: PropTypes.string,
 }
 
