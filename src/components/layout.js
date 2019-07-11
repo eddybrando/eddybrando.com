7a18af320ad0ee780ac407f461d8b8e588371421
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2019-07-11 07:22:13 +0200

    feat(welcome): set main container grid

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,10 +4,10 @@ import PropTypes from "prop-types"
 import Header from "./header"
 
 const Layout = ({ children }) => (
-  <>
+  <div className="page-grid">
     <Header />
     <main className="main-container">{children}</main>
-  </>
+  </div>
 )
 
 Layout.propTypes = {
