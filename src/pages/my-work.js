511279fb2d5e2b7b58b8e1fd027171ea71ff2889
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2019-07-13 11:41:04 +0200

    feat(my-work): display list of companies

--- src/pages/my-work.js
+++ src/pages/my-work.js
@@ -3,41 +3,63 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
-  <Layout
-    headerTitle={{ prepend: "My ", highlight: "work" }}
-    mainContainerClass="main-container--full-height"
-    nav={[{ label: "Home", link: "/" }]}
-  >
-    <SEO
-      description="This is my work as a developer, designer and entrepreneur specialised in web
+const SecondPage = () => {
+  const companies = [
+    { company: "IBM", role: "Web developer, UI/UX designer" },
+    { company: "KICKZ", role: "Web developer, architect, UI/UX designer" },
+    { company: "IBM", role: "Web developer" },
+    {
+      company: "Body Change",
+      role: "Web developer, UI designer, project manager",
+    },
+    {
+      company: "Peruvian Embassy in Ecuador",
+      role: "Web developer, UI/UX designer, graphic designer",
+    },
+  ]
+
+  return (
+    <Layout
+      headerTitle={{ prepend: "My ", highlight: "work" }}
+      mainContainerClass="main-container--full-height"
+      nav={[{ label: "Home", link: "/" }]}
+    >
+      <SEO
+        description="This is my work as a developer, designer and entrepreneur specialised in web
           technologies."
-      title="My work"
-    />
+        title="My work"
+      />
 
-    <section className="how-it-began">
-      <h2>How it began</h2>
-      <p>
-        I started coding around 2003 at age 13 and drafted my first web designs
-        when I was 14.
-      </p>
+      <section className="how-it-began">
+        <h2>How it began</h2>
+        <p>
+          I started coding around 2003 at age 13 and drafted my first web
+          designs when I was 14.
+        </p>
 
-      <p>
-        Back then, my tech stack was HTML 4, CSS 2 and Macromedia Flash 7, along
-        with the occasional JavaScript snippet.
-      </p>
+        <p>
+          Back then, my tech stack consisted of HTML 4, CSS 2 and Macromedia
+          Flash 7, along with the occasional JavaScript snippet.
+        </p>
 
-      <p>
-        My tools of choice were Macromedia Dreamweaver and Microsoft FrontPage
-        for code editing, and Yahoo! GeoCities or the free .tk domains host for
-        hosting.
-      </p>
-    </section>
+        <p>
+          My tools of choice were Macromedia Dreamweaver and Microsoft FrontPage
+          for code editing, and Yahoo! GeoCities or the free .tk domains host
+          for hosting.
+        </p>
+      </section>
 
-    <section>
-      <h2>Some companies I've worked for</h2>
-    </section>
-  </Layout>
-)
+      <section>
+        <h2>Some companies I've worked for</h2>
+      </section>
+
+      {companies.map(({ company, role }, index) => (
+        <div>
+          {company} {role}
+        </div>
+      ))}
+    </Layout>
+  )
+}
 
 export default SecondPage
