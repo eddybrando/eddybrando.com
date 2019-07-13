97025da9b177c5b78ebdb90863b08cd49739449a
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2019-07-13 22:17:49 +0200

    feat(404): pass header information

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,13 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
-  <Layout>
+  <Layout
+    headerTitle={{
+      prepend: "",
+      highlight: "Oops",
+      append: ", 404 over here!",
+    }}
+  >
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
