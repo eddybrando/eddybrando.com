2982df423d7987be998bb2dd123e4e01c46ada21
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2019-07-14 15:44:13 +0200

    feat(contact): add input fields

--- src/pages/contact-me.js
+++ src/pages/contact-me.js
@@ -12,7 +12,25 @@ const ContactMe = () => (
       description="Get in touch with me by using this awesome online form! (It's free!)"
       title="Contact me"
     />
-    Contact form
+    <div className="form-field">
+      <label>Name</label>
+      <input className="field"></input>
+    </div>
+    <div className="form-field">
+      <label>Company</label>
+      <input className="field"></input>
+    </div>
+    <div className="form-field">
+      <label>E-mail address</label>
+      <input className="field"></input>
+    </div>
+    <div className="form-field">
+      <label>Message</label>
+      <input className="field"></input>
+    </div>
+    <a href="#" class="link-block">
+      Send
+    </a>
   </Layout>
 )
 
