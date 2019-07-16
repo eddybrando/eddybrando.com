0fce05076edbaa75d28a0e7b1ca809a537a468b6
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2019-07-16 20:21:55 +0200

    feat(contact): point form to backend

--- src/pages/contact-me.js
+++ src/pages/contact-me.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react"
+import axios from "axios"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -6,13 +7,29 @@ class ContactMeComponent extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      name: "",
       count: 0,
     }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({ name: event.target.value })
   }
 
   handleSubmit(e) {
     e.preventDefault()
     this.setState({ count: this.state.count + 1 })
+    axios
+      .post(`${process.env.GATSBY_API_URL}/contact`, { captchaToken: 123 })
+      .then(res => {
+        console.log(res)
+      })
+      .catch(rej => {
+        console.error(rej)
+      })
   }
 
   render() {
@@ -32,7 +49,11 @@ class ContactMeComponent extends Component {
         <form className="contact-form">
           <div className="form-field">
             <label>Name</label>
-            <input className="field" />
+            <input
+              className="field"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
           </div>
           <div className="form-field">
             <label>Company</label>
@@ -46,12 +67,8 @@ class ContactMeComponent extends Component {
             <label>Message</label>
             <textarea className="field" rows="8" />
           </div>
-          <a
-            href="#"
-            onClick={this.handleSubmit.bind(this)}
-            className="link-block"
-          >
-            Send {this.state.count}
+          <a href="#" onClick={this.handleSubmit} className="link-block">
+            Send {this.state.count} {this.state.name}
           </a>
         </form>
       </Layout>
