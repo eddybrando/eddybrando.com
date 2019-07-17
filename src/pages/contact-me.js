4186f91897308bf691b2d9f51db5e31390ca5d80
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2019-07-17 15:50:27 +0200

    feat(contact): display success message after sending

--- src/pages/contact-me.js
+++ src/pages/contact-me.js
@@ -12,6 +12,7 @@ class ContactMeComponent extends Component {
     super(props)
     this.state = {
       isSubmitting: false,
+      isSubmitted: false,
       company: "",
       email: "",
       errors: {},
@@ -62,8 +63,7 @@ class ContactMeComponent extends Component {
         qs.stringify({ captchaToken, ...payload })
       )
       .then(res => {
-        console.log(res)
-        this.setState({ isSubmitting: false })
+        this.setState({ isSubmitted: true, isSubmitting: false })
       })
       .catch(error => {
         if (error.response && error.response.data) {
@@ -74,12 +74,16 @@ class ContactMeComponent extends Component {
   }
 
   render() {
-    const { isSubmitting, errors, lastValues } = this.state
+    const { isSubmitted, isSubmitting, errors, lastValues } = this.state
 
     return (
       <Layout
         headerTitle={{ highlight: "Contact", append: " me" }}
-        mainContainerClass="main-container--full-height"
+        mainContainerClass={
+          isSubmitted
+            ? `main-container--centered`
+            : `main-container--full-height`
+        }
         nav={[
           { label: "Home", link: "/" },
           { label: "My work", link: "/my-work" },
@@ -89,105 +93,115 @@ class ContactMeComponent extends Component {
           description="Get in touch with me by using this awesome online form! (It's free!)"
           title="Contact me"
         />
-        <form className="contact-form">
-          <div className="form-field">
-            <label>Name</label>
-            <input
-              className={`field ${
-                errors.name && lastValues.name === this.state.name
-                  ? "has-error"
-                  : ""
-              }`}
-              disabled={isSubmitting}
-              name="name"
-              onChange={this.handleChange}
-              value={this.state.name}
-            />
-            {errors.name && lastValues.name === this.state.name && (
-              <div className="field-error-message">{errors.name[0]}</div>
-            )}
+        {isSubmitted ? (
+          <div>
+            <h2>Awesome!</h2>
+            <p>I'll get back to you ASAP.</p>
           </div>
-          <div className="form-field">
-            <label>Company</label>
-            <input
-              className={`field ${
-                errors.company && lastValues.company === this.state.company
-                  ? "has-error"
-                  : ""
-              }`}
-              disabled={isSubmitting}
-              name="company"
-              onChange={this.handleChange}
-              value={this.state.company}
-            />
-            {errors.company && lastValues.company === this.state.company && (
-              <div className="field-error-message">{errors.company[0]}</div>
-            )}
-          </div>
-          <div className="form-field">
-            <label>E-mail address</label>
-            <input
-              className={`field ${
-                errors.email && lastValues.email === this.state.email
-                  ? "has-error"
-                  : ""
-              }`}
-              disabled={isSubmitting}
-              name="email"
-              onChange={this.handleChange}
-              type="email"
-              value={this.state.email}
-            />
-            {errors.email && lastValues.email === this.state.email && (
-              <div className="field-error-message">{errors.email[0]}</div>
-            )}
-          </div>
-          <div className="form-field">
-            <label>Message</label>
-            <textarea
-              className={`field ${
-                errors.message && lastValues.message === this.state.message
-                  ? "has-error"
-                  : ""
-              }`}
-              disabled={isSubmitting}
-              name="message"
-              onChange={this.handleChange}
-              rows="8"
-              value={this.state.message}
-            />
-            {errors.message && lastValues.message === this.state.message && (
-              <div className="field-error-message">{errors.message[0]}</div>
-            )}
-          </div>
-
-          {errors.captchaToken && (
-            <div className="field-error-message captcha-error">
-              Robot verification failed! You may be a robot 🤖. Please try again
-              if you're not.
+        ) : (
+          <form className="contact-form">
+            <div className="form-field">
+              <label>Name</label>
+              <input
+                className={`field ${
+                  errors.name && lastValues.name === this.state.name
+                    ? "has-error"
+                    : ""
+                }`}
+                disabled={isSubmitting}
+                name="name"
+                onChange={this.handleChange}
+                value={this.state.name}
+              />
+              {errors.name && lastValues.name === this.state.name && (
+                <div className="field-error-message">{errors.name[0]}</div>
+              )}
             </div>
-          )}
+            <div className="form-field">
+              <label>Company</label>
+              <input
+                className={`field ${
+                  errors.company && lastValues.company === this.state.company
+                    ? "has-error"
+                    : ""
+                }`}
+                disabled={isSubmitting}
+                name="company"
+                onChange={this.handleChange}
+                value={this.state.company}
+              />
+              {errors.company && lastValues.company === this.state.company && (
+                <div className="field-error-message">{errors.company[0]}</div>
+              )}
+            </div>
+            <div className="form-field">
+              <label>E-mail address</label>
+              <input
+                className={`field ${
+                  errors.email && lastValues.email === this.state.email
+                    ? "has-error"
+                    : ""
+                }`}
+                disabled={isSubmitting}
+                name="email"
+                onChange={this.handleChange}
+                type="email"
+                value={this.state.email}
+              />
+              {errors.email && lastValues.email === this.state.email && (
+                <div className="field-error-message">{errors.email[0]}</div>
+              )}
+            </div>
+            <div className="form-field">
+              <label>Message</label>
+              <textarea
+                className={`field ${
+                  errors.message && lastValues.message === this.state.message
+                    ? "has-error"
+                    : ""
+                }`}
+                disabled={isSubmitting}
+                name="message"
+                onChange={this.handleChange}
+                rows="8"
+                value={this.state.message}
+              />
+              {errors.message && lastValues.message === this.state.message && (
+                <div className="field-error-message">{errors.message[0]}</div>
+              )}
+            </div>
 
-          <Recaptcha
-            ref={e => this.setRecaptchaInstance(e)}
-            sitekey="6LdaIK4UAAAAAEod-5aAdjYNLE_Ooko3-5UDtNYp"
-            size="invisible"
-            verifyCallback={this.verifyCallback}
-          />
+            {errors.captchaToken && (
+              <div className="field-error-message captcha-error">
+                Robot verification failed! You may be a robot{" "}
+                <span role="img" aria-label="robot">
+                  🤖
+                </span>
+                . Please try again if you're not.
+              </div>
+            )}
 
-          {isSubmitting ? (
-            <div>Sending...</div>
-          ) : (
-            <a
-              href="#"
-              onClick={this.executeCaptcha}
-              disabled
-              className="link-block"
-            >
-              Send
-            </a>
-          )}
-        </form>
+            <Recaptcha
+              ref={e => this.setRecaptchaInstance(e)}
+              sitekey="6LdaIK4UAAAAAEod-5aAdjYNLE_Ooko3-5UDtNYp"
+              size="invisible"
+              verifyCallback={this.verifyCallback}
+            />
+
+            {isSubmitting ? (
+              <div>Sending...</div>
+            ) : (
+              <a
+                href="#"
+                onClick={this.executeCaptcha}
+                disabled
+                className="link-block"
+              >
+                Send
+              </a>
+            )}
+          </form>
+        )}
       </Layout>
     )
   }
