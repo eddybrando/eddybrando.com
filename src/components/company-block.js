053ce5f7d71cea55faf34617e82093a3fd3952bb
Eddybrando Vásquez <mail@eddybrando.com>
2020-03-08 22:01:58 +0100

    feat(my-work): add more working experience

---
+++ src/components/company-block.js
@@ -0,0 +1,58 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { ArrowUpRight16 } from '@carbon/icons-react';
+import Img from "gatsby-image";
+
+const CompanyBlock = ({ data, date, details, headline, logoImg, lead, linkLabel, linkTo }) => (
+  <div className={`work-company-block ${logoImg ? '' : 'no-img'}`}>
+    { logoImg ? (
+      <div>
+        <Img
+          className="company-logo"
+          fixed={data[logoImg].childImageSharp.fixed}
+        />
+      </div>
+    ): null}
+    <div>
+      <h3>
+        {headline} {date ? <span className="inline-date">({date})</span> : null}
+      </h3>
+      <div className="font-small">{lead}</div>
+      <div className="font-small" style={{ opacity: .4 }}>{details}</div>
+    </div>
+    {date ? <div className="col-date">{date}</div> : null}
+    {linkLabel && linkTo ? <div>
+      <a
+        className="link-block"
+        href={linkTo}
+        rel="noopener noreferrer"
+        target="_blank"
+        title={linkLabel}
+      >
+        {linkLabel} <ArrowUpRight16 className="link-arrow" />
+      </a>
+    </div> : null}
+  </div>
+);
+
+CompanyBlock.propTypes = {
+  data: PropTypes.object,
+  date: PropTypes.string,
+  details: PropTypes.string,
+  headline: PropTypes.string.isRequired,
+  logoImg: PropTypes.string,
+  lead: PropTypes.string.isRequired,
+  linkLabel: PropTypes.string,
+  linkTo: PropTypes.string,
+}
+
+CompanyBlock.defaultProps = {
+  data: {},
+  date: "",
+  details: "",
+  logoImg: "",
+  linkLabel: "",
+  linkTo: "",
+}
+
+export default CompanyBlock;
--- src/pages/my-work.js
+++ src/pages/my-work.js
@@ -1,42 +1,78 @@
 import React from "react"
 import { graphql, StaticQuery } from "gatsby"
-import Img from "gatsby-image"
+import CompanyBlock from "../components/company-block"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 export const PureMyWorkPage = ({ data }) => {
   const companies = [
     {
-      company: "IBM",
       date: "2019 - Present",
+      details: "Tech stack: JavaScript, TypeScript; Angular, Vue.js; RxJs, Vuex; Node.js; GraphQL, MongoDB",
+      headline: "IBM",
+      lead: "Senior web developer (Team lead)",
       logoImg: "logoIbm",
-      role: "Web developer, UI/UX designer",
     },
     {
-      company: "KICKZ",
       date: "2018 - 2019",
+      details: "Tech stack: JavaScript, JSP; Vue.js; Vuex; Java, PHP",
+      headline: "KICKZ (Zalando)",
+      lead: "Lead web developer",
       logoImg: "logoKickz",
-      role: "Web developer, architect, UI/UX designer",
     },
     {
-      company: "IBM",
       date: "2016 - 2018",
+      details: "Tech stack: JavaScript; React, Vue.js; Redux; Node.js",
+      headline: "IBM",
+      lead: "Web developer",
       logoImg: "logoIbm",
-      role: "Web developer",
     },
     {
-      company: "BodyChange",
       date: "2014 - 2015",
+      details: "Tech stack: JavaScript; PHP; WordPress",
+      headline: "BodyChange",
+      lead: "Web developer",
       logoImg: "logoBodychange",
-      role: "Web developer, UI designer, project manager",
     },
     {
-      company: "Peruvian Embassy in Ecuador",
       date: "2008 - 2015",
+      details: "Tech stack: JavaScript; PHP; Joomla",
+      headline: "Peruvian Embassy in Ecuador",
+      lead: "Web developer",
       logoImg: "logoPeru",
-      role: "Web developer, UI/UX designer, graphic designer",
     },
-  ]
+  ];
+
+  const personalProjects = [
+    {
+      headline: "Eddybrando.com",
+      lead: "Professional personal website",
+      details: "React-based PWA generated with GatsbyJS, powered by GraphQL and tested with Jest",
+      linkLabel: "eddybrando.com",
+      linkTo: "https://eddybrando.com",
+    },
+    {
+      headline: "Podcasts en Español",
+      lead: "Social network for podcast enthusiasts",
+      details: "Laravel-powered website with a PHP back end, a front end enhanced with Vue.js, and MySQL databases",
+      linkLabel: "podcastsenespanol.com",
+      linkTo: "https://podcastsenespanol.com/",
+    },
+    {
+      headline: "VioletaCast",
+      lead: "Podcast producer website",
+      details: "Multilingual Laravel website written in PHP and Blade templates",
+      linkLabel: "violetacast.com",
+      linkTo: "https://violetacast.com/en",
+    },
+    {
+      headline: "Psicóloga Edith",
+      lead: "Professional personal website",
+      details: "Vue.js-based website generated with Gridsome and connected to PHP Lumen micro-services",
+      linkLabel: "psicologaedith.com",
+      linkTo: "https://psicologaedith.com/",
+    },
+  ];
 
   return (
     <Layout
@@ -76,22 +112,31 @@ export const PureMyWorkPage = ({ data }) => {
         <h2>Some companies I've worked at</h2>
       </section>
 
-      {companies.map(({ company, date, logoImg, role }, index) => (
-        <div className="work-company-block" key={index}>
-          <div>
-            <Img
-              className="company-logo"
-              fixed={data[logoImg].childImageSharp.fixed}
-            />
-          </div>
-          <div>
-            <h3>
-              {company} <span className="inline-date">({date})</span>
-            </h3>
-            <div className="font-small">{role}</div>
-          </div>
-          <div className="col-date">{date}</div>
-        </div>
+      {companies.map(({ headline, date, logoImg, lead, details }, index) => (
+        <CompanyBlock
+          data={data}
+          date={date}
+          details={details}
+          headline={headline}
+          key={index}
+          lead={lead}
+          logoImg={logoImg}
+        />
+      ))}
+
+      <section className="work-companies">
+        <h2>A few of my personal projects</h2>
+      </section>
+      
+      {personalProjects.map(({ headline, lead, linkLabel, linkTo, details }, index) => (
+        <CompanyBlock
+          details={details}
+          headline={headline}
+          key={index}
+          lead={lead}
+          linkLabel={linkLabel}
+          linkTo={linkTo}
+        />
       ))}
     </Layout>
   )
